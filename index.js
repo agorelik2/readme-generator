242b25b13f6501aed37f5a49ac4a1fdd52f5ede9
Red Fairy <agorelik2@gmail.com>
2020-08-06 20:13:09 -0400

    stated index.js

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
+var inquirer = require("inquirer");
+var fs = require("fs");
+var generateMarkdown = require("./utils/generateMarkdown");
+
 // array of questions for user
 const questions = [
-
+  {
+    type: "input",
+    name: "projectName",
+    message: "What is the name of your project?",
+  },
+  {
+    type: "input",
+    name: "projectDescription",
+    message: "What is the description of your project?",
+  },
+  // ,
+  // {
+  //     type:"input",
+  //     name: 'projectName',
+  //     message: 'What is the name of your project?'
+  // },
+  // {
+  //     type:"input",
+  //     name: 'projectName',
+  //     message: 'What is the name of your project?'
+  // },
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
 function init() {
-
+  inquirer.prompt(questions).then(function (answers) {
+    console.log(answers);
+    console.log(answers.projectDescription);
+    var readmeReturn = generateMarkdown(answers);
+  });
 }
 
 // function call to initialize program
