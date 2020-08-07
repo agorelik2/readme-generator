138922b7f2774bf1a581ed2ee9beba5639285d5c
Red Fairy <agorelik2@gmail.com>
2020-08-06 20:33:38 -0400

    added all prompts

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ var generateMarkdown = require("./utils/generateMarkdown");
 const questions = [
   {
     type: "input",
+    name: "userName",
+    message: "What is your GitHub user name?",
+  },
+  {
+    type: "input",
+    name: "userEmail",
+    message: "What is your user email?",
+  },
+  {
+    type: "input",
     name: "projectName",
     message: "What is the name of your project?",
   },
@@ -14,17 +24,31 @@ const questions = [
     name: "projectDescription",
     message: "What is the description of your project?",
   },
-  // ,
-  // {
-  //     type:"input",
-  //     name: 'projectName',
-  //     message: 'What is the name of your project?'
-  // },
-  // {
-  //     type:"input",
-  //     name: 'projectName',
-  //     message: 'What is the name of your project?'
-  // },
+  {
+    type: "input",
+    name: "licenseType",
+    message: "What type of license do you need?",
+  },
+  {
+    type: "input",
+    name: "installComm",
+    message: "What command should you run to install dependencies?",
+  },
+  {
+    type: "input",
+    name: "runTestComm",
+    message: "What command should be used to run tests?",
+  },
+  {
+    type: "input",
+    name: "useRepo",
+    message: "What user needs to know about using the repo?",
+  },
+  {
+    type: "input",
+    name: "contrRepo",
+    message: "What user needs to know about contributing to the repo?",
+  },
 ];
 
 // function to write README file
